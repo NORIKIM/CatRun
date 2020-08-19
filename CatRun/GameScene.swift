feef5e1a6c2575cb01933678e3a292b3c38cecac
김지나 <jinagold.k@gmail.com>
2020-08-19 21:04:43 +0900

    Background scroll

--- CatRun/GameScene.swift
+++ CatRun/GameScene.swift
@@ -13,13 +13,27 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         createBackground()
     }
+    override func update(_ currentTime: TimeInterval) {
+        scrollBackground()
+    }
     
     func createBackground() {
         for loop in 0 ... 2 {
             let background = SKSpriteNode(imageNamed: "bg")
+            background.name = "bg"
             background.size = CGSize(width: (self.scene?.size.width)!, height: (self.scene?.size.height)!)
             background.position = CGPoint(x: CGFloat(loop) * self.frame.size.width, y: self.frame.size.height / 2)
             self.addChild(background)
         }
+    }
+    
+    func scrollBackground() {
+        self.enumerateChildNodes(withName: "bg", using: ({ (node, error) in
+            node.position.x -= 4
+            
+            if node.position.x < -self.frame.size.width {
+                node.position.x += self.frame.size.width * 3
+            }
+        }))
     }
 }
